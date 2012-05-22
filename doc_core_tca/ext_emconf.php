9b6449466aec121dad88f1a6c702ddaa27223b52
Christopher <chrissitopher@gmx.de>
2012-05-22 16:08:35 +0200

    [TASK] Migrate doc_core_tca 4.6
    Import doc_core_tca for TYPO3 4.6.
    
    Change-Id: I3f3f5996b8139fa2318d6b593b1a9373ab192497

--- doc_core_tca/ext_emconf.php
+++ doc_core_tca/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_tca".
 #
-# Auto generated 04-11-2011 08:58
+# Auto generated 21-11-2011 20:57
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TCA Reference',
-	'description' => 'Complete reference to the Table Control Array ($TCA)',
+	'description' => 'Complete reference to the Table Configuration Array ($TCA)',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.5.2',
+	'version' => '4.6.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"e3b7";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_tca.pdf";s:4:"8a1b";s:20:"doc/doc_core_tca.txt";s:4:"7226";s:14:"doc/manual.sxw";s:4:"cc51";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"7bf8";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_tca.pdf";s:4:"0fe3";s:20:"doc/doc_core_tca.txt";s:4:"fb8d";s:14:"doc/manual.sxw";s:4:"ab39";}',
 	'suggests' => array(
 	),
 );
