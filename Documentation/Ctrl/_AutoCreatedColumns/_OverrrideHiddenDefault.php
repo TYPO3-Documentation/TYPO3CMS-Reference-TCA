06d774d0a12a9c0bf3c3f47d06aac49fc868a99d
lina.wolf <lwolf@w-commerce.de>
2024-07-13 16:22:20 +0200

    [FEATURE] Add default TCA columns from ctrl #967
    Releases: main
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/967

---
+++ Documentation/Ctrl/_AutoCreatedColumns/_OverrrideHiddenDefault.php
@@ -0,0 +1,4 @@
+<?php
+
+// New pages are disabled by default
+$GLOBALS['TCA']['pages']['columns']['disabled']['config']['default'] = 1;
---
+++ Documentation/Ctrl/_AutoCreatedColumns/_pages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'ctrl' => [
+        'enablecolumns' => [
+            'disabled' => 'disabled',
+        ],
+    ],
+    // ...
+];
