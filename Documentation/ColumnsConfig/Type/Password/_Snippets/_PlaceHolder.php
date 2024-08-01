7bddb34981edcae26de1d3e0447ba71e4144a5c2
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-08-01 14:09:06 +0200

    [TASK] Merge properties to password page (#1123)
    Releases: main, 12.4

---
+++ Documentation/ColumnsConfig/Type/Password/_Snippets/_PlaceHolder.php
@@ -0,0 +1,10 @@
+<?php
+
+$temporaryColumns['aColorField'] = [
+    'title' => 'My color field',
+    'config' => [
+        'type' => 'color',
+        'placeholder' => '#FF8700',
+        'mode' => 'useOrOverridePlaceholder',
+    ],
+];
