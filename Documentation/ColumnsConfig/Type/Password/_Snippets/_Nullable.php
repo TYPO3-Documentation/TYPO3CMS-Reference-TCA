857a760c8a6f7504f06a7fbe8d95b0e3d4e34eee
lina.wolf <lwolf@w-commerce.de>
2024-08-01 14:06:03 +0200

    [TASK] Merge properties to password page
    Releases: main, 12.4

---
+++ Documentation/ColumnsConfig/Type/Password/_Snippets/_Nullable.php
@@ -0,0 +1,11 @@
+<?php
+
+$passwordField = [
+    'config' => [
+        'title' => 'A nullable field',
+        'config' => [
+            'type' => 'password',
+            'nullable' => true,
+        ],
+    ],
+];
