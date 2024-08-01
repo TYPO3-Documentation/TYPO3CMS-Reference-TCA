4dcebe70d4f10561ac2388c3285942633e62818b
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-08-01 04:25:19 +0200

    [TASK] Merge properties to number page (#1120)
    * [TASK] Merge properties to number page
    
    Releases: main, 12.4
    
    * [TASK] Merge properties to number page
    
    Releases: main, 12.4

---
+++ Documentation/ColumnsConfig/Type/Number/_Properties/_SliderUnlimited.php
@@ -0,0 +1,11 @@
+<?php
+
+$temporaryColumns['numberField'] = [
+    'label' => 'A number between 0 and 10 000',
+    'config' => [
+        'type' => 'number',
+        'slider' => [
+            'step' => 1,
+        ],
+    ],
+];
