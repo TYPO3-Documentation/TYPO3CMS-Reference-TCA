6dec0ced0355d39bd639c1bb33959742bbec188d
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-08-02 10:25:22 +0200

    [TASK] Merge properties to text - codeEditor page (#1132)
    Releases: main, 12.4

---
+++ Documentation/ColumnsConfig/Type/Text/T3Editor/_Snippets/_Format.php
@@ -0,0 +1,12 @@
+<?php
+
+$temporaryColumns['my_editor'] = [
+    'label' => 'My HTML Editor',
+    'description' => 'field description',
+    'config' => [
+        'type' => 'text',
+        'renderType' => 'codeEditor',
+        'format' => 'html',
+        'rows' => 7,
+    ],
+];
