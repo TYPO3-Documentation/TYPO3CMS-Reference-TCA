d5c15b23bfed5008ac91d1eb1d42d120b37395fc
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-09-10 19:31:32 +0200

    [FEATURE] Add types > creationOptions > defaultValues (#1166)
    * [FEATURE] Add types > creationOptions > defaultValues
    
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/801
    Resolves: https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-TCA/issues/1161
    Releases: main
    
    * Update Documentation/Types/_CodeSnippets/_creationOptions.php
    
    Co-authored-by: Stefan Frömken <froemken@gmail.com>
    
    * [FEATURE] Add types > creationOptions > defaultValues
    
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/801
    Resolves: https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-TCA/issues/1161
    Releases: main
    
    ---------
    
    Co-authored-by: Stefan Frömken <froemken@gmail.com>

--- Documentation/Types/_CodeSnippets/_creationOptions.php
+++ Documentation/Types/_CodeSnippets/_creationOptions.php
@@ -8,6 +8,9 @@ $GLOBALS['TCA']['tt_content'] = array_merge_recursive(
             'div' => [
                 'creationOptions' => [
                     'saveAndClose' => true,
+                    'defaultValues' => [
+                        'bodytext' => '<p>some text</p>',
+                    ],
                 ],
             ],
         ],
