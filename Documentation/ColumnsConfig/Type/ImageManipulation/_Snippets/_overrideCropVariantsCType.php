c84ee0c2bd432c594ab89984a126d6858abb7367
alexander-nitsche-governikus <161829362+alexander-nitsche-governikus@users.noreply.github.com>
2025-11-30 06:20:36 +0100

    Fixes of the image manipulation chapter (#1350)
    * [DOCS] Vary examples of image manipulation per content element
    
    * [DOCS] Remove outdated docs of disabling image manipulation fields
    
    Since the default crop variants get replaced instead of being
    extended, this example seems to be outdated.
    
    See https://github.com/TYPO3/typo3/blob/13.4/typo3/sysext/backend/Classes/Form/Element/ImageManipulationElement.php#L231.

--- Documentation/ColumnsConfig/Type/ImageManipulation/_Snippets/_overrideCropVariantsCType.php
+++ Documentation/ColumnsConfig/Type/ImageManipulation/_Snippets/_overrideCropVariantsCType.php
@@ -1,9 +1,9 @@
 <?php
 
-$GLOBALS['TCA']['tt_content']['columns']['image']['config']['overrideChildTca']['columns']['crop']['config'] = [
+$GLOBALS['TCA']['tt_content']['types']['textpic']['columnsOverrides']['image']['config']['overrideChildTca']['columns']['crop']['config'] = [
     'cropVariants' => [
         'mobile' => [
-            'title' => 'LLL:EXT:ext_key/Resources/Private/Language/locallang.xlf:imageManipulation.mobile',
+            'title' => 'LLL:my_extension.db:imageManipulation.mobile',
             'cropArea' => [
                 'x' => 0.1,
                 'y' => 0.1,
